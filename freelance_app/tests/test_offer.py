692b522132e9e06518f2b7a356dd5d2f7929e4b5
Marcel Göhn <marcelgoehn@googlemail.com>
2025-12-28 15:40:55 +0100

    test: added tests for offer detail get view

--- freelance_app/tests/test_offer.py
+++ freelance_app/tests/test_offer.py
@@ -227,3 +227,14 @@ class OfferTests(APITestCase):
         url = reverse("offer-detail", kwargs={"pk": self.offer.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_offer_not_authenticated(self):
+        url = reverse("offer-detail", kwargs={"pk": self.offer.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_retrieve_offer_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("offer-detail", kwargs={"pk": 1233212133321321321321})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
