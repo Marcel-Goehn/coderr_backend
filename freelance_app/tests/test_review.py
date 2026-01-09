25a18c9068a154473d7ba51a2ab0759e31299acf
Marcel Göhn <marcelgoehn@googlemail.com>
2026-01-09 19:42:43 +0100

    test: added tests for review list get method

--- freelance_app/tests/test_review.py
+++ freelance_app/tests/test_review.py
@@ -92,3 +92,14 @@ class ReviewTests(APITestCase):
         }
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_review_list_successful(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("review-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_review_list_not_authenticated(self):
+        url = reverse("review-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
