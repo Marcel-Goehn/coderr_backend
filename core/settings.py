e09aaee3339645d00366b2de8ebfd1f14fba33a2
Marcel Göhn <marcelgoehn@googlemail.com>
2026-03-22 12:18:25 +0100

    refactor: Changed debug to False for production

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,10 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'django-insecure-1rpod!98!86zb4j1!&&u)7e92+@_m^l@0)=y0ff8)0=vk=(l$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'coderr-backend.marcelgoehn.de'
 ]
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
